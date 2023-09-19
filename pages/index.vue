be7b0630859c62aaa3809c55146770adb700599f
jcyenyen <amanda7776@gmail.com>
2023-09-19 15:46:23 +0800

    Tailwind安裝及測試

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,25 @@
 <template>
-  <div>
-    <h1>{{ title }}</h1>
+  <div class="box">
+    <button
+      class="btn-primary text-[25px]"
+    >
+      測試用
+    </button>
   </div>
 </template>
-<script setup>
-const title = ref('hello world')
-
-</script>
+<script setup></script>
 <style>
-
+/* .box {
+    width: 384px;
+    margin: 16px auto;
+    background-color: rgb(6 182 212);
+    padding: 24px;
+  }
+  button {
+    width: 100px;
+    border-radius: 4px;
+    border: 1px solid black;
+    background-color: white;
+    padding: 8px;
+  } */
 </style>
--- pages/test/test.vue
+++ pages/test/test.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="p-6 bg-sky-500">
         <h1>{{ title }}</h1>
     </div>
     <TestCom />
